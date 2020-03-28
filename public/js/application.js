aae4f45aa6c9c446b817e8c58cf552a45b8cc4fc
40175139 <40175139@live.napier.ac.uk>
2020-03-28 23:27:47 +0000

    basic working app

---
+++ public/js/application.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#HC-table tr th button").click(function(e){
+        var $button = $(this);
+        var $dateFrom = $button.siblings('input[name="dateFrom"]');
+        var $dateTo = $button.siblings('input[name="dateTo"]');
+        console.log($dateFrom.val());
+        console.log($dateTo.val());
+        if ($dateFrom.val() == "" || $dateTo.val() == ""){
+            alert ("Fill in");
+            return;
+        }
+        var data = {dateFrom:$dateFrom.val(), dateTo:$dateTo.val()};
+        $.post( "/home", data, function( data ) {
+            $("<p>" + data + "</p>").insertAfter($button);
+        }, "text");
+    });
+});
